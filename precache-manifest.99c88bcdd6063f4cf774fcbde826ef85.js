fa71120c8c3aacabc26450b9af976963025d1d38
Abdullahi <abdullahihassanke@gmail.com>
2020-03-04 17:08:01 +0300

    Updates

---
+++ precache-manifest.99c88bcdd6063f4cf774fcbde826ef85.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "301308012a1fe37f149d57906c1fab44",
+    "url": "/Trident-Solutions/index.html"
+  },
+  {
+    "revision": "610310201ed1ab564142",
+    "url": "/Trident-Solutions/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "18d95a71d62448695f69",
+    "url": "/Trident-Solutions/static/css/main.d04bd3cc.chunk.css"
+  },
+  {
+    "revision": "610310201ed1ab564142",
+    "url": "/Trident-Solutions/static/js/2.bd45a936.chunk.js"
+  },
+  {
+    "revision": "95fe7b47190d8ffb46ace11b00922862",
+    "url": "/Trident-Solutions/static/js/2.bd45a936.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "18d95a71d62448695f69",
+    "url": "/Trident-Solutions/static/js/main.372c3736.chunk.js"
+  },
+  {
+    "revision": "5a5b712cac38ace0fbd4",
+    "url": "/Trident-Solutions/static/js/runtime-main.67cad224.js"
+  },
+  {
+    "revision": "05797719cfdc67989199c0f1b84fe80a",
+    "url": "/Trident-Solutions/static/media/image2.05797719.svg"
+  },
+  {
+    "revision": "d408954892cc88889c746394ddf3f0dd",
+    "url": "/Trident-Solutions/static/media/img2.d4089548.svg"
+  },
+  {
+    "revision": "74a81cfd914bafd1543b1e6cc7a2efeb",
+    "url": "/Trident-Solutions/static/media/img3.74a81cfd.svg"
+  }
+]);
